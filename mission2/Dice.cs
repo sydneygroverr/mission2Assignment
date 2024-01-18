13f1416858a7862e7e7c6c0bcaa13c74a33cfa41
sydneygroverr <sydney.grover8@gmail.com>
2024-01-17 17:12:02 -0700

    Add project files.

---
+++ mission2/Dice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mission2
+{
+    public class Dice
+    {
+        // declare new array to hold the percent of total rolls for each outcome
+        public int[] rollPercents = new int[10];
+
+        // create array to track roll outputs
+        public int[] rollOutputs = new int[10];
+
+        public int[] Roll(int numRolls)
+        {
+            // create an instance of Random
+            Random random = new Random();
+
+            int[] results = new int[11];
+
+            for (int i = 0; i < numRolls; i++)
+            {
+
+                // includes the first number but goes until the second num/doesnt include it
+                int dice1 = random.Next(1, 7);
+                int dice2 = random.Next(1, 7);
+
+                int totalRoll = dice1 + dice2;
+
+                results[totalRoll - 2]++;
+
+            }
+            return results;
+
+        }
+
+    }
+}
---
+++ mission2/Program.cs
@@ -0,0 +1,39 @@
+﻿using mission2;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    private static void Main(string[] arks)
+    {
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+        string timesRolled = Console.ReadLine();
+
+        // convert the string to an int
+        int numRolls = int.Parse(timesRolled);
+
+        // create new Dice instance
+        Dice dice = new Dice();
+
+        // simulate rolls annd get results
+        int[] results = dice.Roll(numRolls);
+
+      
+        Console.WriteLine("Results:");
+
+        int totalRolls = numRolls;
+        Console.WriteLine("Total rolls = " +  totalRolls);
+
+        for (int i = 0; i < results.Length; i++)
+        {
+            int percentage = results[i] * 100 / totalRolls;
+            string asterisks = new string('*', percentage);
+            Console.WriteLine($"{i + 2}: {asterisks}");
+        }
+
+        Console.WriteLine("\nThank you for playing!");
+    }
+
+
+
+}
